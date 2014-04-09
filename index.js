68f3843262a447d4182048f14540a020043a8fcf
Michael Williams <dinosaur@riseup.net>
2014-04-08 23:34:20 -0700

    write handleErrors helper

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var Url = require('url');
 var APP_ID = "52b8025a";
 var APP_KEY = "d5929f45ba3383b8cfcf6bb1bca70da4";
 
-var getMetadata = function (options) {
+var getMetadata = function (options, callback) {
   var options = options || {};
   var query = options.query || '*';
   var start = options.query || '0';
@@ -25,11 +25,21 @@ var getMetadata = function (options) {
     },
   });
 
-  request(url, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      console.log(body) // Print the api content
-    }
-  });
+  request(url, callback);
 };
 
-getMetadata();
+var handleErrors = function (callback) {
+  return function (error, response, body) {
+    if (error) { throw error; }
+    if (response.statusCode !== 200) {
+      throw new Error("status code not OK:", response.statusCode);
+    }
+    // call callback async
+    process.nextTick(function () { callback(body); });
+  };
+};
+
+
+getMetadata({}, handleErrors(function (body) {
+  console.log(body) // Print the api content
+}));
